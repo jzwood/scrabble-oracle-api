0f168ab7d06badc623f8b129e9b2dcd8645f2d53
Jake Wood <jakew@captricity.com>
2020-10-02 10:20:43 -0400

    url route defined

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,11 @@
 
 import Web.Scotty
 import Network.HTTP.Types
-import Article
 
 main = scotty 3000 $ do
-  -- get article (json)
-  get "/article" $ do
-    json $ Article 13 "caption" "content" -- Call Article constructor and encode the result as JSON
-
-  -- post article (json)
-  post "/article" $ do
-    article <- jsonData :: ActionM Article -- Decode body of the POST request as an Article object
-    json article                           -- Send the encoded object back as JSON
+  get (regex "^/board/([A-Z_]{255})/rack/([A-Z]{7})$") $ do
+    board <- param "1"
+    rack <- param "2"
+    text $ mconcat [board, rack]
+  notFound $ do
+   text "there is no such route."
