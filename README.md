149fe7db43e79eb3aab28bfd4ca8af6a8d0012ff
Jake Wood <jakew@captricity.com>
2020-10-12 09:10:20 -0400

    adds json response

--- README.md
+++ README.md
@@ -1,10 +1,33 @@
 # Scrabble-Scotty Web App
 
+API wrapper for [Scrabble AI](https://github.com/jzwood/ScrabbleOracle).
 
+
+# local
+
+```haskell
+stack install
+scrabble-scotty-app-exe
+```
+
+hit api via CURL or Postman:
+```haskell
+localhost:3000/board/<board>/rack/<rack>
+params:
+  - board:
+    * 225 characters of scrabble board (left to right and top to bottom)
+    * uppercase
+    * empty space = '_'
+  - rack
+    * 7 characters of rack (5-7 allowed)
+    * uppercase
+```
+
+# deploy
 
 ## Docker
 ### Build
 `docker build -t "scrabble-scotty-app:Dockerfile" .`
 
 ### Run
-`docker run -it --user root <tag> /bin/bash`
+`docker run -it <tag> /bin/bash`
--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+import GHC.Generics
 
 import Control.Applicative
 import Data.Char (toUpper)
 import Data.Maybe (fromMaybe)
 import Web.Scotty
+import Data.Aeson
 import Network.HTTP.Types
 import Game.SingleBestPlay
 import Game.ScrabbleBoard
@@ -13,6 +17,15 @@ import qualified Data.Text.Lazy as L
 up :: String -> String
 up = map toUpper
 
+
+data OracleAPIOutput = OracleAPIOutput { board :: String
+                                          , word :: String
+                                          , score :: Score
+                                          } deriving (Generic, Show)
+
+instance ToJSON OracleAPIOutput where
+    toJSON (OracleAPIOutput board word score) =
+      object ["board" .= board, "word" .= word, "score" .= score]
 
 main = scotty 3000 $ do
   get (regex "^/board/([a-zA-Z1234_]{225})/rack/([a-zA-Z]{5,7})$") $
@@ -24,5 +37,5 @@ main = scotty 3000 $ do
           maybeIOBestPlay = liftA2 makeSinglePlay board rack
           bestPlay = fromMaybe (return (nullBoard, "", 0)) maybeIOBestPlay
       (b, w, s) <- liftAndCatchIO bestPlay
-      text $ L.pack (stringifyBoard b)
+      Web.Scotty.json $ OracleAPIOutput (stringifyBoard b) w s
   notFound $ text "there is no such route."
