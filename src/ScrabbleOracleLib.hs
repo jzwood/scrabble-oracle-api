9179b6d51d51a7b91853e43c8f8318c73988ecb2
Jake Wood <jakew@captricity.com>
2020-10-22 22:55:56 -0400

    can send test email via postman -- hooray

--- src/ScrabbleOracleLib.hs
+++ src/ScrabbleOracleLib.hs
@@ -3,9 +3,12 @@
 module ScrabbleOracleLib where
 
 import Control.Applicative
+import Control.Lens
 import Data.Aeson
+import qualified Data.ByteString.Lazy as BL
 import Data.Time.Clock.POSIX
 import GHC.Generics
+import Network.Wreq
 import Web.Scotty
 
 import Prelude
@@ -30,10 +33,25 @@ writeToFile str = do
   P.writeFile ("./media/board_" ++ time ++ ".txt") str
   return ()
 
+sendEmail :: String -> String -> IO (Response BL.ByteString)
+sendEmail to html = postWith opts url [ "from" := ("Scrabble Oracle App <mailgun@sandboxc9f2eaeb1dc741a6b5e0849924393f85.mailgun.org>" :: String)
+                                      , "to" := to
+                                      , "subject" := ("Scrabble Oracle Best Play" :: String)
+                                      , "html":= html
+                                      , "require_tls" := ("true" :: String)
+                                      ]
+  where
+    opts = defaults & auth ?~ basicAuth "api" "d9535fa5255cc199c13e54d16549239d-53c13666-3f24bd99"
+    url = "https://api.mailgun.net/v3/sandboxc9f2eaeb1dc741a6b5e0849924393f85.mailgun.org/messages"
+
 -- saves output to disc when mail gun api ENV is not set
 emailBestPlay :: String -> String -> IO ()
 emailBestPlay strBoard strRack =
   do
     (b, w, s) <- getBestPlay strBoard strRack
-    writeToFile $ unlines [stringifyBoard b, w, show s]
+    let board = stringifyBoard b
+        word = w
+        score = show s
+    --writeToFile $ unlines [stringifyBoard b, w, show s]
+    sendEmail "jzwood14@gmail.com" $ unlines [board, word, score]
     return ()
